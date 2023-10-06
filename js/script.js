ad4fdce7c36320bbc7604a79f81442549fb76a95
Dezmond <zeppelin95@ya.ru>
2023-10-06 19:04:59 +0700

    default commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
   
+  const serviceNames = [
+    'ASSETS & PROPS',
+    '3D CHARACTERS',
+	  'NFT COLLECTIONS',
+	  '3D LOCATIONS',
+	  '3D ANIMATIONS',
+	  'GAME PLAY TRAILERS'
+  ]
+
+
+  // $("#service-1").modal({
+  //   fadeDuration: 500
+  // });
   
-  
+
+
 });
