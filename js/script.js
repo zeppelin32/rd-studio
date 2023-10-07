5c8339266709f972a00cc08d414ce6c4b138ecd4
Dezmond <zeppelin95@ya.ru>
2023-10-07 17:01:50 +0700

    default commit

--- js/script.js
+++ js/script.js
@@ -9,11 +9,33 @@ $(document).ready(function () {
 	  'GAME PLAY TRAILERS'
   ]
 
-  $(body).overlayScrollbars();
-
   // $("#service-1").modal({
   //   fadeDuration: 500
   // });
+
+  const heightForChange = 200;
+
+  $(document).on( "scroll", function() {
+
+    const heightNow = window.scrollY;
+    const opacity = Math.min(heightNow/heightForChange, 1);
+
+    $('#header-bg').css({"opacity": `${opacity}`});
+    $('#header-container').css({"padding": `${25 - (10*opacity)}px 40px`});
+    $('#navbar-img').css({"width": `${90 - (20*opacity)}px`});
+
+    
+    // $('#header-bg').css({"opacity": "0.5"});
+
+    // const box = document.getBoundingClientRect();
+
+    // return {
+    //   top: box.top + pageYOffset,
+    //   left: box.left + pageXOffset
+    // };
+
+    console.log(opacity)
+  } );
   
 
 
